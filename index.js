b1fb3797918b1d10c0c7372ef322b5decb6c1a52
Joseph Mwanzia <makamwanzia@gmail.com>
2024-12-25 12:19:25 +0300

    feat: select difficulty level

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 #!/usr/bin/env node
+const inquirer = require('inquirer');
 
-console.log("Welcome to the Number Guessing Game!")
+console.log("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.\nYou have 5 chances to guess the correct number.\n")
+
+inquirer
+  .prompt([
+    {
+      name: 'difficulty',
+      type: 'rawlist',
+      message: 'Please select the difficulty level:',
+      choices: [
+        { name: 'Easy (10 chances)', value: 'one' },
+        { name: 'Medium (5 chances)', value: 'two' },
+        { name: 'Hard (3 chances)', value: 'three' },
+      ],
+    },
+  ])
+  .then((answers) => {
+    console.info('Answer:', answers.difficulty);
+  })
+  .catch((err) => {
+    console.log(err)
+  })
