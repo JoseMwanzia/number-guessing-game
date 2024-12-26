dc75cac5e5c18b79f71741d206d52b87709913a2
Joseph Mwanzia <makamwanzia@gmail.com>
2024-12-26 12:54:32 +0300

    feat: guess an input number

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 #!/usr/bin/env node
 const inquirer = require('inquirer');
-
 console.log("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.\nYou have 5 chances to guess the correct number.\n")
 
-inquirer
-  .prompt([
-    {
-      name: 'difficulty',
-      type: 'rawlist',
-      message: 'Please select the difficulty level:',
-      choices: [
-        { name: 'Easy (10 chances)', value: 'one' },
-        { name: 'Medium (5 chances)', value: 'two' },
-        { name: 'Hard (3 chances)', value: 'three' },
-      ],
+const difficultyLevels = {
+  1: 'Easy',
+  2: 'Medium',
+  3: 'Hard',
+};
+
+inquirer.prompt([
+  {
+    name: 'difficulty',
+    type: 'rawlist',
+    message: 'Enter your choice:',
+    choices: [
+      { name: 'Easy (10 chances)', value: 1 },
+      { name: 'Medium (5 chances)', value: 2 },
+      { name: 'Hard (3 chances)', value: 3 },
+    ],
+  },
+  {
+    name: 'Enter your guess',
+    type: 'number',
+    message: (answers) => {console.log(`Great! You have selected the ${difficultyLevels[answers.difficulty]} difficulty level.\nLet's start the game!`)},
+  },
+  {
+    name: 'Enter your guess',
+    type: 'number',
+    message: (answers) => {
+      console.log(answers);
     },
-  ])
-  .then((answers) => {
-    console.info('Answer:', answers.difficulty);
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+  },
+])
+
